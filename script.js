5afbaf8d22a127f234d8429bd4916dfa96d48c84
KevinMAnderson <kma2k4@hotmail.com>
2017-09-14 23:27:32 -0600

    created functions to add step snd ingredient inputs on forms of addRecipe page and func to create object from addRecipe data

--- script.js
+++ script.js
@@ -4,78 +4,144 @@
 
         // 'use strict';
 
-/* MATERIALIZE UTILITIES  *****************************************************************************************************************************************************/
+        /* MATERIALIZE UTILITIES  *****************************************************************************************************************************************************/
 
-    $( '.button-collapse' ).sideNav( {
-        closeOnClick: true
-    } );
-    $( 'select' ).material_select();
-    $('select').material_select('destroy');
-    $( '.parallax' ).parallax();
+        $( '.button-collapse' ).sideNav( {
+            closeOnClick: true
+        } );
+        $( 'select' ).material_select();
+        $( 'select' ).material_select( 'destroy' );
+        $( '.parallax' ).parallax();
 
-/* GLOBAL VARIABLES   *****************************************************************************************************************************************************/
+        /* GLOBAL VARIABLES   *****************************************************************************************************************************************************/
 
-    // heroku DB url
-    const dbUrl = 'https://shielded-oasis-62403.herokuapp.com/';
+        // heroku DB url
+        const dbUrl = 'https://shielded-oasis-62403.herokuapp.com/';
 
-    //jQuery
-    $createReviewForm = $('#createReviewForm');
-    $recipePageRecipe = $('#recipePageRecipe');
-    $createReviewName= $('#createReviewName');
-    $createReviewText= $('#createReviewText');
+        //jQuery
+        // Add Review Page
+        $createReviewForm = $( '#createReviewForm' );
+        $recipePageRecipe = $( '#recipePageRecipe' );
+        $createReviewName = $( '#createReviewName' );
+        $createReviewText = $( '#createReviewText' );
+        // add Recipe Page
+        $addRecipeForm = $( '#addRecipeForm' );
+        $addRecipeTitle = $( '#addRecipeTitle' );
+        $addRecipeName = $( '#addRecipeName' );
+        $addRecipeText = $( '#addRecipeText' );
+        $addRecipeFile = $( '#addRecipeFile' );
+        $addRecipeIngredient = $( '.addRecipeIngredient' );
+        $addRecipeIngredientInput = $('#addRecipeIngredientInput');
+        $addRecipeIngredDiv = $('#addRecipeIngredDiv');
+        $addRecipeStep = $('#addRecipeStep');
+        $addRecipeStepInput =$('#addRecipeStepInput');
+        $addRecipeStepDiv = $('#addRecipeStepDiv');
 
 
-    // Creat Review Form
 
-    let createReviewData= {};
+        // Creat Review Form
+
+        let createReviewData = {};
+
+        let createRecipeData = {};
 
 
-/* FUNCTIONS  *****************************************************************************************************************************************************/
+        /* FUNCTIONS  *****************************************************************************************************************************************************/
 
-    // Creat object from form inputs to match DB Review table columns
-    function createReviewDataObjForDB () {
-            createReviewData.rating =$('input[name=rating]:checked').val();
-            createReviewData.text= $createReviewText.val();
-            createReviewData.user_name= $createReviewName.val();
-            createReviewData.recipe_id = $recipePageRecipe.attr('data-recipeId');
-            createReviewData = JSON.stringify(createReviewData);
-            console.log(createReviewData);
+        // Creat object from form inputs to match DB Review table columns
+        function createReviewDataObjForDB() {
+            createReviewData.rating = $( 'input[name=rating]:checked' ).val();
+            createReviewData.text = $createReviewText.val();
+            createReviewData.user_name = $createReviewName.val();
+            createReviewData.recipe_id = $recipePageRecipe.attr( 'data-recipeId' );
+            createReviewData = JSON.stringify( createReviewData );
+        }
+
+        // Creat object from form inputs to match DB RECIPE table columns
+        function createRecipeDataObjForDB() {
+            createRecipeData.name = $addRecipeTitle.val();
+            createRecipeData.person = $addRecipeName.val();
+            createRecipeData.text = $addRecipeText.val();
+            createRecipeData.image = $addRecipeFile.val();
+            createRecipeData.ingredients = $addRecipeIngredient;
+            createRecipeData.steps = $
+            createRecipeData = JSON.stringify( createRecipeData );
         }
 
 
+        // Create Ingredient input items
+        function createIngredientInput () {
+            let number = Number($addRecipeIngredDiv.length) + 1;
+            let newIngredientInput = `<div class="input-field col s12">
+                <input id="addRecipeIngredient${number}" placeholder="add ingredient" type="text" class="validate addRecipeIngredient">
+                <label for="addRecipeIngredient${number}">Ingredient</label>
+            </div>`;
+            return newIngredientInput;
+        }
 
-/* AJAX  *****************************************************************************************************************************************************/
+        // Create Step input items
+        function createStepInput () {
+            let number = Number($addRecipeStepDiv.length) + 1;
+            let newStepInput = `<div class="input-field col s12">
+                <input id="addRecipeStep${number}" placeholder="add ingredient" type="text" class="validate addRecipeStep">
+                <label for="addRecipeStep${number}">Step</label>
+            </div>`;
+            return newStepInput;
+        }
 
-    function sendCreateReviewData () {
+
+        /* AJAX  *****************************************************************************************************************************************************/
+
+        function sendCreateReviewData() {
 
 
-        $.ajax( {
-            type: "POST",
-            url: `${dbUrl}review/createReview`,
-            data: createReviewData,
-            crossDomain: true,
-            success: function ( response ) {
-                ///show thank you message div & hide order form
-                response(alert('success'));
-            }
+            $.ajax( {
+                type: "POST",
+                url: `${dbUrl}review/createReview`,
+                data: createReviewData,
+                crossDomain: true,
+                success: function ( response ) {
+                    ///show thank you message div & hide order form
+                    response( alert( 'success' ) );
+                }
+            } );
+
+        }
+
+        /* EVENT LISTENERS  *****************************************************************************************************************************************************/
+
+        $createReviewForm.submit( ( event ) => {
+            event.preventDefault();
+
+            createReviewDataObjForDB();
+            sendCreateReviewData();
         } );
 
-    }
+        $addRecipeForm.submit( ( event ) => {
+            event.preventDefault();
 
-/* EVENT LISTENERS  *****************************************************************************************************************************************************/
+            createRecipeDataObjForDB();
 
-    $createReviewForm.submit((event)=>{
-        event.preventDefault();
-
-        createReviewDataObjForDB();
-        sendCreateReviewData ();
-    });
+        } );
 
 
+        $addRecipeIngredientInput.click(()=>{
+
+            $addRecipeIngredDiv.append(createIngredientInput);
+
+        });
+
+        $addRecipeStepInput.click(()=>{
+
+            $addRecipeStepDiv.append(createStepInput);
+
+        });
 
 
 
-/* FUNCTION CALLS *****************************************************************************************************************************************************/
+
+
+        /* FUNCTION CALLS *****************************************************************************************************************************************************/
 
 
 
