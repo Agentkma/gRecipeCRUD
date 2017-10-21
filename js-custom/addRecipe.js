901b7f73b698c302626b0cbac7d89c6d70112cea
KevinMAnderson <kma2k4@hotmail.com>
2017-10-20 19:18:15 -0600

    fixed addRecipe page so can add and remove ingredients and steps...added Tyler Torres info to footer of index

--- js-custom/addRecipe.js
+++ js-custom/addRecipe.js
@@ -1,189 +1,158 @@
-
-'use strict';
+"use strict";
 // ( function ( $ ) {
 
-    $( document ).ready( function () {
+$(document).ready(function() {
+	/* MATERIALIZE UTILITIES  *****************************************************************************************************************************************************/
+
+	$(".button-collapse").sideNav({
+		closeOnClick: true
+	});
+	$("select").material_select();
+	// $( 'select' ).material_select( 'destroy' );
+	$(".parallax").parallax();
+	$(".chips-initial").material_chip("data");
+
+	/* GLOBAL VARIABLES   *****************************************************************************************************************************************************/
+
+	// add Recipe Page
+	const $addRecipeForm = $("#addRecipeForm");
+	const $addRecipeTitle = $("#addRecipeTitle");
+	const $addRecipeName = $("#addRecipeName");
+	const $addRecipeText = $("#addRecipeText");
+	const $addRecipeFile = $("#addRecipeFile");
+	const $addStepDiv = $("#addStepDiv");
+	const $childrenAddStepDiv = $("#addStepDiv > .addEachStepDiv");
+	const $addEachStepDiv = $(".addEachStepDiv");
+	const $addRecipeIngBtn = $("#addRecipeIngBtn");
+	const $addRecipeIngredient = $(".addRecipeIngredient");
+	const $addStepBtn = $(".addStepBtn");
+	const $addIngredientDiv = $("#addIngredientDiv");
+	const $addRecipeStep = $("#addRecipeStep");
+	const $addRecipeStepInput = $("#addRecipeStepInput");
+	const $addRecipeStepDiv = $("#addRecipeStepDiv");
+	let $deleteItem = $(".close");
+
+	const addRecipeDataObj = {
+		title: $addRecipeTitle.val(),
+		recipeDescription: $addRecipeText.val(),
+		// TODO need to find the google service that will turn pic file uploads to urls
+		file: $addRecipeFile.val(),
+		personName: $addRecipeName.val(),
+		ingredients: getAllIngredients()
+	};
+
+	let createRecipeData = {};
+
+	/* FUNCTIONS  *****************************************************************************************************************************************************/
+
+	function getAllIngredients() {
+		let $allIngredientElements = $addIngredientDiv.find(
+			".addRecipeIngredient"
+		);
+
+		let allIngredients = Array.prototype.map.call(
+			$allIngredientElements,
+			input => {
+				// TODO CHANGE SO EACH ingredient has name, unit, amount
+				return {
+					ingredientName: input.value,
+					unit: "",
+					amount: ""
+				};
+			}
+		);
+		return allIngredients;
+	}
+
+	// HANDLEBAR
+	function addRecipeIng() {
+		//grab template
+		const source = $("#addIngTemplate").html();
+		//ready handlebars...by loading source/template
+		const template = Handlebars.compile(source);
+		let idIncrNum = $addIngredientDiv.length;
+		// console.log(idIncrNum)
+		const data = { id: idIncrNum };
+		const html = template(data);
+		$addIngredientDiv.append( html);
+		$deleteItem = $(".close");
+		$("select").material_select();
+	}
+
+	// Create Step input group/row
+	function createNewStep() {
+		//grab template
+		const source = $("#addStepTemplate").html();
+		//ready handlebars...by loading source/template
+		const template = Handlebars.compile(source);
+		let idIncrNum = $addStepDiv.length;
+		// console.log(idIncrNum)
+		const data = { id: idIncrNum };
+		const html = template(data);
+		$addStepDiv.append(html);
+		$deleteItem = $(".close");
+		$("select").material_select();
+	}
+
+	// Create object from form inputs to match DB RECIPE table columns
+	function createRecipeObject() {
+		//TODO NEED to get quantites to match ingreidents..
+		// { ingredient: 'beef',
+		// quantity : {
+		//     unit : lbs,
+		//     amount: 2
+		// }}
+		// createRecipeData.quantities
+
+		let $allStepElements = $addRecipeStepIngQty.find(
+			".addRecipeIngredient"
+		);
+		// TODO steps need order and description data
+		let allSteps = Array.prototype.map.call($allStepElements, input => {
+			return {
+				steps: {
+					order: input.value,
+					stepDescription: "get data"
+				}
+			};
+		});
+		createRecipeData.steps = allSteps;
+
+		// console.log(createRecipeData);
+		createRecipeData = JSON.stringify(createRecipeData);
+	}
+
+	function deleteItem() {
+
+	}
+
+	/* EVENT LISTENERS  *****************************************************************************************************************************************************/
+
+	$addRecipeForm.submit(event => {
+		event.preventDefault();
+		createRecipeObject();
+	});
+
+	$addRecipeIngBtn.click(() => {
+		addRecipeIng();
+		$("select").material_select();
+	});
+
+	$addStepBtn.click(() => {
+		createNewStep();
+			$deleteItem = $(".close");
+		$("select").material_select();
+	});
+
+
+	$(document).on('click','.close',function(event) {
+		console.log('Test Click');
+	 $(event.target).closest(".row").remove()
+	});
 
 
 
-/* MATERIALIZE UTILITIES  *****************************************************************************************************************************************************/
 
-$( '.button-collapse' ).sideNav( {
-    closeOnClick: true,
-} );
-$( 'select' ).material_select();
-// $( 'select' ).material_select( 'destroy' );
-$( '.parallax' ).parallax();
-$('.chips-initial').material_chip('data');
-
-
-/* GLOBAL VARIABLES   *****************************************************************************************************************************************************/
-
-    // add Recipe Page
-    const $addRecipeForm = $( '#addRecipeForm' );
-    const $addRecipeTitle = $( '#addRecipeTitle' );
-    const $addRecipeName = $( '#addRecipeName' );
-    const $addRecipeText = $( '#addRecipeText' );
-    const $addRecipeFile = $( '#addRecipeFile' );
-    const $addStepDiv =$('#addStepDiv');
-    const $childrenAddStepDiv= $('#addStepDiv > .addEachStepDiv')
-    const $addEachStepDiv = $('.addEachStepDiv');
-    const $addRecipeIngBtn = $('#addRecipeIngBtn');
-    const $addRecipeIngredient = $( '.addRecipeIngredient' );
-    const $addIngredientDiv = $('#addIngredientDiv');
-    const $addRecipeStep = $('#addRecipeStep');
-    const $addRecipeStepInput =$('#addRecipeStepInput');
-    const $addRecipeStepDiv = $('#addRecipeStepDiv');
-
-
-
-    let createRecipeData = {};
-
-
-/* FUNCTIONS  *****************************************************************************************************************************************************/
-
-
-
-
-
-
-
-    // Add Recipe Form
-    function getAllIngredients (){
-        let $allIngredientElements =             $addRecipeStepIngQty.find('.addRecipeIngredient');
-
-    let allIngredients = Array.prototype.map.call($allIngredientElements,(input)=>{
-        // TODO CHANGE SO EACH ingredient has name, unit, amount
-        return {ingredientName:input.value };
-
-    });
-
-        return allIngredients;
-    }
-
-    // HANDLEBAR
-    function addRecipeIng () {
-
-        //grab template
-        const source   = $("#addRecipeTemplate").html();
-        //ready handlebars...by loading source/template
-        const template = Handlebars.compile(source);
-        let idIncrNum = $addIngredientDiv.length;
-        // console.log(idIncrNum)
-        const data = {id: idIncrNum};
-        const html = template(data);
-        $addIngredientDiv.append(html);
-
-    }
-
-    // Create Step input group/row
-    function createNewStep () {
-        //BUG cannot get correct numb of elements with number below
-        // TODO revise so ingredient name, qty, unit separate from step
-        // TODO REFACTOR TO USE HANDLEBARS
-        let number = Number($addStepDiv.length);
-        console.log($addStepDiv, $addStepDiv.length);
-        let newStepInput =
-        `<div class="row addEachStepDiv">
-                <div class="stepTags">
-                    <div class="chip light-blue">
-                        <i class="close">${number}</i>
-                    </div>
-                    <div class="chip red">
-                        <i class="close material-icons">delete</i>
-                    </div>
-                </div>
-
-                <div class="input-field col s12">
-                    <input placeholder="add step" id="addRecipeStep${number}" type="text" class="validate addRecipeStep">
-                    <label for="addRecipeStep${number}">Step</label>
-                </div>
-
-                <div class="input-field col s12 m6">
-                    <input id="addRecipeIngredient${number}" placeholder="add ingredient" type="text" class="validate addRecipeIngredient">
-                    <label for="addRecipeIngredient${number}">Ingredient</label>
-                </div>
-                <div class="input-field col s12 m2">
-                    <input id="addRecipeQuanity${number}" placeholder="add quantity" type="number" class="validate addRecipeQuantity">
-                    <label for="addRecipeQuanity${number}">Quantity</label>
-                </div>
-                <div class="input-field col s12 m4">
-                    <select id="addRecipeUnit${number}">
-                <option value="" disabled selected>Choose your option</option>
-                <option value="1">Ounces</option>
-                <option value="2">Lbs</option>
-                <option value="3">Ts</option>
-                <option value="4">Tbls</option>
-                <option value="5">Cups</option>
-                <option value="6">Quarts</option>
-              </select>
-                    <label>Select Unit</label>
-                </div>
-            </div>`;
-
-        return newStepInput;
-    }
-
-    // Create object from form inputs to match DB RECIPE table columns
-    function createRecipeDataObjForDB() {
-        const addRecipeDataObj = {
-            title : $addRecipeTitle.val(),
-            recipeDescription: $addRecipeText.val(),
-            // need to find the google service that will turn pic file uploads to urls
-            file: $addRecipeFile.val(),
-            personName : $addRecipeName.val(),
-            ingredients: getAllIngredients(),
-        };
-    //TODO NEED to get quantites to match ingreidents..
-    // { ingredient: 'beef',
-        // quantity : {
-        //     unit : lbs,
-        //     amount: 2
-        // }}
-        // createRecipeData.quantities
-
-        // TODO steps need order and description data
-
-        let $allStepElements =  $addRecipeStepIngQty.find('.addRecipeIngredient');
-
-        let allSteps = Array.prototype.map.call($allStepElements,(input)=>{
-            return {steps: {
-            	order: input.value,
-            	stepDescription: 'get data',
-                }
-            };
-        });
-        createRecipeData.steps = allSteps;
-
-        // console.log(createRecipeData);
-        createRecipeData = JSON.stringify( createRecipeData );
-
-    }
-
-
-
-/* EVENT LISTENERS  *****************************************************************************************************************************************************/
-
-$addRecipeForm.submit( ( event ) => {
-    event.preventDefault();
-
-    createRecipeDataObjForDB();
-
-} );
-
-
-$addRecipeIngBtn.click(()=>{
-        addRecipeIng();
-        //TODO cannot append the addRecipeIng html....tested using a string and still not working
-        console.log($addIngredientDiv)
-    $( 'select' ).material_select();
-
+	/* FUNCTION CALLS *****************************************************************************************************************************************************/
 });
-
-    /* DYNAMIC RENDERING *****************************************************************************************************************************************************/
-
-
-
-
-    });
 // });
--- js-custom/script.js
+++ js-custom/script.js
@@ -1,139 +1,159 @@
-( function ( $ ) {
-'use strict';
-    $( document ).ready( function () {
+(function($) {
+	"use strict";
+	$(document).ready(function() {
+		/* MATERIALIZE UTILITIES  *****************************************************************************************************************************************************/
+		// TODO  CAN these materialize utilities be called with a function in all js pages?
+		$(".button-collapse").sideNav({
+			closeOnClick: true
+		});
+		$("select").material_select();
+		// $( 'select' ).material_select( 'destroy' );
+		$(".parallax").parallax();
+		$(".chips-initial").material_chip("data");
 
+		/* GLOBAL VARIABLES   *****************************************************************************************************************************************************/
 
+		// heroku DB url
+		// const dbUrl = 'https://shielded-oasis-62403.herokuapp.com/';
+		// recipes from DB TODO
+		let recipes = [];
 
-/* MATERIALIZE UTILITIES  *****************************************************************************************************************************************************/
-    // TODO  CAN these materialize utilities be called with a function in all js pages?
-$( '.button-collapse' ).sideNav( {
-    closeOnClick: true,
-    } );
-$( 'select' ).material_select();
-// $( 'select' ).material_select( 'destroy' );
-$( '.parallax' ).parallax();
-$('.chips-initial').material_chip('data');
+		//jQuery
 
+		// Index/home Page
+		const $index_section_recipes = $("#indexRecipes");
+		/* FUNCTIONS  *****************************************************************************************************************************************************/
 
+		// HANDLEBAR Helper Func**************
 
+		Handlebars.registerHelper("compare", function(
+			lvalue,
+			operator,
+			rvalue,
+			options
+		) {
+			if (arguments.length < 3)
+				throw new Error(
+					"Handlerbars Helper 'compare' needs 2 parameters"
+				);
 
+			// var operator = options.hash ? option.hash.operator : ">=";
 
-/* GLOBAL VARIABLES   *****************************************************************************************************************************************************/
+			var operators = {
+				"==": function(l, r) {
+					return l == r;
+				},
+				"===": function(l, r) {
+					return l === r;
+				},
+				"!=": function(l, r) {
+					return l != r;
+				},
+				"<": function(l, r) {
+					return l < r;
+				},
+				">": function(l, r) {
+					return l > r;
+				},
+				"<=": function(l, r) {
+					return l <= r;
+				},
+				">=": function(l, r) {
+					return l >= r;
+				},
+				typeof: function(l, r) {
+					return typeof l == r;
+				}
+			};
+
+			if (!operators[operator])
+				throw new Error(
+					"Handlerbars Helper 'compare' doesn't know the operator " +
+						operator
+				);
+
+			var result = operators[operator](lvalue, rvalue);
+
+			if (result) {
+				//true
+				return options.fn(this);
+			} else {
+				//false
+				return options.inverse(this);
+			}
+		});
+
+		function addRecipesToIndex(data) {
+			// INDEX PAGE RECIPES TEMPLATE
+			//grab template
+			const source = $("#recipeIndexTemplate").html();
+			//ready handlebars...by loading source/template
+			const template = Handlebars.compile(source);
+
+			// TODO change context to be recipes from database
+			var data = {
+				recipe: [
+					{
+						recipedId: 1,
+						recipeImg: "img/beefRibs.jpeg",
+						recipeName: "BBQ Rib Mania",
+						starRating: 1,
+						recipeDescription:
+							"This is the best recipe ever.  Great ribs and great life.  This is the best recipe ever.  Great ribs and great life."
+					},
+					{
+						recipedId: 2,
+						recipeImg: "img/beefRibs.jpeg",
+						recipeName: "BBQ Rib HooHa",
+						starRating: 3,
+						recipeDescription:
+							"This is the best recipe ever.  Great ribs and great life.  This is the best recipe ever.  Great ribs and great life."
+					},
+					{
+						recipeId: 5,
+						recipeImg: "img/beefRibs.jpeg",
+						recipeName: "BBQ Rib Jeepers",
+						starRating: 4,
+						recipeDescription:
+							"This is the best recipe ever.  Great ribs and great life.  This is the best recipe ever.  Great ribs and great life."
+					}
+				]
+			};
+			const html = template(data);
+			$index_section_recipes.append(html);
+		}
+
+		/* AJAX  *****************************************************************************************************************************************************/
+
+		// settings
+		//
+		// var settings = {
+		//   "async": true,
+		//   "crossDomain": true,
+		//   "url": "https://shielded-oasis-62403.herokuapp.com/",
+		//   "method": "POST",
+		//   "headers": {
+		//     "content-type": "application/json",
+		//     "cache-control": "no-cache",
+		//     "postman-token": "f5a6b99f-9ebc-4b78-8a5a-6cdd06815a73"
+		//   },
+		//   "processData": false,
+		//   "data": `${daTa}`
+		// }
+
+		//
+		// $.ajax(settings).done(function (response) {
+		//   console.log(response);
+		// });
+
+		/* EVENT LISTENERS  *****************************************************************************************************************************************************/
+
+		/* FUNCTION CALLS *****************************************************************************************************************************************************/
+		addRecipesToIndex();
+	}); // end of document ready
+})(jQuery); // end of jQuery name space
+
+/* SHARED FUNCTIONS/VARS *****************************************************************************************************************************************************/
 
 // heroku DB url
-// const dbUrl = 'https://shielded-oasis-62403.herokuapp.com/';
-// recipes from DB TODO
-let recipes=[];
-
-//jQuery
-
-// Index/home Page
-const $index_section_recipes = $('#indexRecipes');
-/* FUNCTIONS  *****************************************************************************************************************************************************/
-
-
-// HANDLEBAR Helper Func**************
-
-
-    Handlebars.registerHelper('compare', function(lvalue,operator, rvalue, options) {
-        if (arguments.length < 3)
-            throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
-
-        // var operator = options.hash ? option.hash.operator : ">=";
-
-        var operators = {
-            '==':       function(l,r) { return l == r; },
-            '===':      function(l,r) { return l === r; },
-            '!=':       function(l,r) { return l != r; },
-            '<':        function(l,r) { return l < r; },
-            '>':        function(l,r) { return l > r; },
-            '<=':       function(l,r) { return l <= r; },
-            '>=':       function(l,r) { return l >= r; },
-            'typeof':   function(l,r) { return typeof l == r; }
-        }
-
-        if (!operators[operator])
-            throw new Error("Handlerbars Helper 'compare' doesn't know the operator "+operator);
-
-        var result = operators[operator](lvalue,rvalue);
-
-        if( result ) {
-            //true
-            return options.fn(this);
-        } else {
-            //false
-            return options.inverse(this);
-        }
-    });
-
-    function addRecipesToIndex (data) {
-        // INDEX PAGE RECIPES TEMPLATE
-        //grab template
-        const source   = $("#recipeIndexTemplate").html();
-        //ready handlebars...by loading source/template
-        const template = Handlebars.compile(source);
-
-        // TODO change context to be recipes from database
-        var data = { recipe: [
-                {recipedId: 1, recipeImg: "img/beefRibs.jpeg",recipeName: "BBQ Rib Mania", starRating: 1,
-                recipeDescription: "This is the best recipe ever.  Great ribs and great life.  This is the best recipe ever.  Great ribs and great life."},
-                {recipedId: 2, recipeImg: "img/beefRibs.jpeg",recipeName: "BBQ Rib HooHa", starRating: 3,
-                recipeDescription: "This is the best recipe ever.  Great ribs and great life.  This is the best recipe ever.  Great ribs and great life."},
-                {recipeId:5,recipeImg: "img/beefRibs.jpeg",recipeName: "BBQ Rib Jeepers", starRating: 4,
-                recipeDescription: "This is the best recipe ever.  Great ribs and great life.  This is the best recipe ever.  Great ribs and great life."}]}
-        const html = template(data);
-        $index_section_recipes.append(html);
-    }
-
-
-/* AJAX  *****************************************************************************************************************************************************/
-
-
-        // settings
-        //
-            // var settings = {
-            //   "async": true,
-            //   "crossDomain": true,
-            //   "url": "https://shielded-oasis-62403.herokuapp.com/",
-            //   "method": "POST",
-            //   "headers": {
-            //     "content-type": "application/json",
-            //     "cache-control": "no-cache",
-            //     "postman-token": "f5a6b99f-9ebc-4b78-8a5a-6cdd06815a73"
-            //   },
-            //   "processData": false,
-            //   "data": `${daTa}`
-            // }
-
-
-
-//
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-// });
-
-/* EVENT LISTENERS  *****************************************************************************************************************************************************/
-
-
-
-
-/* DYNAMIC RENDERING *****************************************************************************************************************************************************/
-
-addRecipesToIndex();
-
-
-
-/* FUNCTION CALLS *****************************************************************************************************************************************************/
-
-
-
-
-    } ); // end of document ready
-} )( jQuery ); // end of jQuery name space
-
-
-
-
-
-// heroku DB url
-const dbUrl = 'https://shielded-oasis-62403.herokuapp.com/';
+const dbUrl = "https://shielded-oasis-62403.herokuapp.com/";
